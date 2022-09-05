3153500fa1a98f0cd1d0f54de631b88940c8c290
Endor H <86890810+endorh@users.noreply.github.com>
2022-09-05 16:00:30 +0200

    🚀 Add GitHub Packages repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,8 +28,9 @@ plugins {
 
 // Mod info --------------------------------------------------------------------
 
-val modGroup = "endorh.simpleconfig"
 val modId = "simpleconfig"
+val modGroup = "endorh.simpleconfig"
+val githubRepo = "endorh/simpleconfig"
 val apiVersion = "1.0.0"
 val modVersion = "1.0.2"
 val mcVersion = "1.19.2"
@@ -409,6 +410,14 @@ artifacts {
 
 publishing {
     repositories {
+        maven("https://maven.pkg.github.com/$githubRepo") {
+            name = "GitHubPackages"
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+        
         maven(rootProject.projectDir.parentFile.resolve("maven")) {
             name = "LocalMods"
         }
