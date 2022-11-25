fdbf2ea6f072ffafbaf2f48e477e319508702fec
Endor H <86890810+endorh@users.noreply.github.com>
2022-11-25 13:50:44 +0100

    🩹 Expose `TranslatedEnum` in the API

--- src/main/java/endorh/simpleconfig/core/entry/EnumEntry.java
+++ src/main/java/endorh/simpleconfig/core/entry/EnumEntry.java
@@ -5,6 +5,7 @@ import com.google.common.collect.Lists;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import endorh.simpleconfig.api.ConfigEntryHolder;
 import endorh.simpleconfig.api.entry.EnumEntryBuilder;
+import endorh.simpleconfig.api.entry.EnumEntryBuilder.TranslatedEnum;
 import endorh.simpleconfig.config.ClientConfig.advanced;
 import endorh.simpleconfig.core.AbstractConfigEntry;
 import endorh.simpleconfig.core.AbstractConfigEntryBuilder;
@@ -26,7 +27,10 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -85,13 +89,6 @@ public class EnumEntry<E extends Enum<E>>
 		}
 	}
 	
-	public interface ITranslatedEnum {
-		Component getDisplayName();
-		default List<Component> getHelpTooltip() {
-			return Collections.emptyList();
-		}
-	}
-	
 	public String presentName(E value) {
 		String name = value.name();
 		String lowerCaseName = name.toLowerCase();
@@ -133,7 +130,7 @@ public class EnumEntry<E extends Enum<E>>
 				  ? Component.literal("✔ ").withStyle(ChatFormatting.DARK_AQUA)
 				  : Component.literal("_ ").withStyle(ChatFormatting.DARK_AQUA));
 			}
-			boolean correct = defValue instanceof ITranslatedEnum
+			boolean correct = defValue instanceof EnumEntryBuilder.TranslatedEnum
 			                  || Arrays.stream(enumClass.getEnumConstants())
 			                    .allMatch(e -> I18n.exists(getEnumTranslationKey(e)));
 			status = status.append(
@@ -150,7 +147,7 @@ public class EnumEntry<E extends Enum<E>>
 	@OnlyIn(Dist.CLIENT) @Override protected void addTranslationsDebugInfo(List<Component> tooltip) {
 		super.addTranslationsDebugInfo(tooltip);
 		if (parent != null) {
-			if (defValue instanceof ITranslatedEnum)
+			if (defValue instanceof EnumEntryBuilder.TranslatedEnum)
 				tooltip.add(Component.literal(" + Enum provides its own translations").withStyle(
 				  ChatFormatting.GRAY));
 			tooltip.add(
@@ -160,7 +157,7 @@ public class EnumEntry<E extends Enum<E>>
 				final MutableComponent status =
 				  I18n.exists(key)
 				  ? Component.literal("(✔ present)").withStyle(ChatFormatting.DARK_GREEN)
-				  : (defValue instanceof ITranslatedEnum)
+				  : (defValue instanceof EnumEntryBuilder.TranslatedEnum)
 				    ? Component.literal("(not present)").withStyle(ChatFormatting.DARK_GRAY)
 				    : Component.literal("(✘ missing)").withStyle(ChatFormatting.RED);
 				tooltip.add(Component.literal("   > ").withStyle(ChatFormatting.GRAY)
@@ -182,8 +179,8 @@ public class EnumEntry<E extends Enum<E>>
 	
 	@OnlyIn(Dist.CLIENT)
 	protected Component enumName(E item) {
-		if (item instanceof ITranslatedEnum)
-			return ((ITranslatedEnum) item).getDisplayName();
+		if (item instanceof EnumEntryBuilder.TranslatedEnum)
+			return ((TranslatedEnum) item).getDisplayName();
 		final String key = getEnumTranslationKey(item);
 		if (I18n.exists(key))
 			return Component.translatable(key);
@@ -192,8 +189,8 @@ public class EnumEntry<E extends Enum<E>>
 	
 	@OnlyIn(Dist.CLIENT)
 	protected List<Component> enumTooltip(E item) {
-		if (item instanceof ITranslatedEnum)
-			return ((ITranslatedEnum) item).getHelpTooltip();
+		if (item instanceof EnumEntryBuilder.TranslatedEnum)
+			return ((TranslatedEnum) item).getHelpTooltip();
 		return optSplitTtc(getEnumTooltipKey(item));
 	}
 	
